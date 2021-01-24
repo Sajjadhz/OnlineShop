4a728f59f6c2fe15b5fbd61b00ddc4638b10349a
Sajjadhz <30774845+Sajjadhz@users.noreply.github.com>
2021-01-24 11:25:18 +0330

    Update settings.py
    remove secrec_key from setting

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ww*n_&#6+oz0^-8m#r$(giml#rxcul1#le4zj)0pj@5)l4nwt-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
